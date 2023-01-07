04dfa173c7221755855415d0e02fbff028ab117b
higorj <higor_sgomes@outlook.com>
2023-01-07 14:17:38 -0300

    Finalizacao projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Nokia n = new Nokia("Nokia", "3310", "1234", 124);
+n.Ligar();
+n.InstalarAplicativo("Jogo da cobrinha");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Iphone i = new Iphone("Iphone", "2G", "1235", 64);
+i.ReceberLigacao();
+i.InstalarAplicativo("Zap");
